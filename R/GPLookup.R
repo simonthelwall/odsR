e67e177db01572f29dea2a8525662932d8ed25e0
Georgina Anderson <georgina.anderson@phe.gov.uk>
2018-05-18 14:27:23 +0100

    Got working GPLookup function - needs editing to cope with non-standard ODS rcord field names

--- R/GPLookup.R
+++ R/GPLookup.R
@@ -1,52 +1,58 @@
 
 # retrieve org IDs and API URLs for all Active GP Practices (only retrives first 1000)
 
-# Work In Progress
+# Work In Progress - TO DO
+# **********  NEED TO BIND PAGES so working with full GP list *************
+# Need to account for records with operational and legal start and end dates
+# Need to account for eecords with only one parent (colname = extension not extension 1)
 
-
-# test practice : getODSfull("A81008") # this practice clodes 31st jan 2018
 
 
 # retrieve all GP practices - **********  NEED TO BIND PAGES *************
 allgps <- getODS(PrimaryRoleId="RO177",NonPrimaryRoleId="RO76")
 
-# create loop
+# Create empty Lookup dataframe
+lkup <- setNames(data.frame(matrix(ncol = 4, nrow = 0)),
+                 c("GPPRAC15CD","GPPRAC15NM","CCG15CD", "CCG15NM"))
 
-activegp <- data.frame()
+# loop through each GP Practice record - error at record 38 - parent field just extension not extension1
 
-for (i in (1:nrow(allgps))) {
+for (i in (1:37)) {  #nrow(allgps))) {
 
-    thisgp    <- getODSfull(allgps[i,2])
-    lkp        <- data.frame(matrix(thisgp,nrow=1),stringsAsFactors=FALSE) %>%
-                 select(6,1,2,53)  # need to identify best end date field or combine with status and last change date?
-    names(lkp) <- c("GPPracticeCode","GPPracticeName","CCGCode")  # data lake principles say keep names as source but can't as parent name doesn't have a 'name'
-    ccgname    <- getODSfull(lkp[3])[1]
+    live <- 0
 
-    activegp <- rbind(thisgp,activegp)
+    thisgp <- t(getODSfull(allgps[i,2]))
+
+    if("Organisation.Date.Start" %in% colnames(thisgp)) {
+
+        if ("Organisation.Date.End" %in% colnames(thisgp)) {
+           lkup1  <- data.frame(matrix(thisgp,nrow=1, dimnames=dimnames(thisgp)),stringsAsFactors=FALSE) %>%
+                       select(Organisation.OrgId.extension,
+                              Organisation.Name,
+                              Organisation.Rels.Rel.Target.OrgId.extension1,
+                              Organisation.Date.Start,
+                              Organisation.Date.End)
+           if (lkup1$Organisation.Date.Start <= "2015-12-31" &
+               (is.na(lkup1$Organisation.Date.End) | lkup1$Organisation.Date.End > "2015-12-31")) {
+               live <- 1
+           }
+
+        } else {
+           lkup1  <- data.frame(matrix(thisgp,nrow=1, dimnames=dimnames(thisgp)),stringsAsFactors=FALSE) %>%
+                        select(Organisation.OrgId.extension,
+                          Organisation.Name,
+                           Organisation.Rels.Rel.Target.OrgId.extension1,
+                          Organisation.Date.Start)
+           if (lkup1$Organisation.Date.Start <= "2015-12-31") {
+               live <- 1
+           }
+        }
+    }
+    # append live organisation data to lkup dataframe
+    if (live == 1) {
+        ccgname    <- getODSfull(lkup1[3])[1]
+        names(lkup1) <- c("GPPRAC15CD","GPPRAC15NM","CCG15CD", "CCG15NM")
+        lkup1      <- data.frame(lkup1[1:3],CCG15Name = ccgname)
+        lkup       <- rbind(lkup,lkup1)
+    }
 }
-
-
-
-
-# development - code below creates lookup for single medical practice
-
-# get ODS code to input to getODSfull function (Woodseats medical Centre)
-#org1  <- getODS(Name="Woodseats",Status="Active")[5,2]
-
-# return full record for practice
-#full1 <- getODSfull(org1)
-
-# create data frame and select practice code and name and parent code
-#df1 <- data.frame(matrix(full1,nrow=1),stringsAsFactors=FALSE) %>%
-#    select(6,1,53)
-
-# name columns
-#names(df1) <- rownames(full1)[c(6,1,53)]
-#names(df1) <- c("GPPracticeCode","GPPracticeName","CCGCode")
-
-# get parent organisation name
-parent1 <- getODSfull(df1[3])[1]
-
-lkup1 <- data.frame(df1,CCGName = parent1)
-
-
